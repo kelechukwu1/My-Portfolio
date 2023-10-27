20785c64a3557a1e4c091e2fe492785eb9251390
Kelechukwu <paparazzip8@gmail.com>
2023-10-28 00:35:51 +0100

    added springy-text feature

--- src/components/AboutFew.jsx
+++ src/components/AboutFew.jsx
@@ -1,17 +1,23 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import SpringyText from "./springy-text/SpringyText";
 
 const AboutFew = () => {
 	return (
 		<>
 			<div className="mx-4 md:mx-24 h-full md:py-24 flex items-center">
 				<div className="md:grid md:grid-cols-2 md:gap-4 md:justify-between items-center">
-					<div className="space-y-10">
-						<div className="text-3xl md:text-4xl lg:text-5xl font-black text-center md:text-start">
-							A Little About Me
+					<div className="mt-5 md:mt-0">
+						<div className="mb-8 md:mb-0 text-3xl md:text-4xl lg:text-5xl font-black text-center md:text-start">
+							<SpringyText
+								className="justify-center"
+								text="A Little About Me"
+							/>
 						</div>
-						<div className="text-center md:text-start md:text-[1.1rem] text-muted-main fluid-lg">
+						{/* <SpringyText className="justify-center" text="A Little About Me" /> */}
+
+						<div className="text-center mb-8 md:mb-0 md:text-start md:text-[1.1rem] text-muted-main fluid-lg">
 							Focused on building awesome and high-quality websites that are
 							also visually appealing and easy to use. I have a strong
 							foundation in HTML, CSS, and JavaScript, and I'm proficient in
@@ -19,7 +25,7 @@ const AboutFew = () => {
 							to improve my skills and stay up-to-date on the latest front-end
 							technologies.
 						</div>
-						<div className="flex justify-center md:justify-start">
+						<div className="flex justify-center mb-8 md:mb-0 md:justify-start">
 							<Link href={"/about"}>
 								<button className="text-center px-12 py-6 rounded-full bg-stone-800 hover:bg-blue-700 transition duration-500">
 									More About Me
--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -1,12 +1,18 @@
 import React from "react";
+import SpringyText from "./springy-text/SpringyText";
 
 const Greeting = () => {
 	return (
 		<div className="mx-4 md:mx-24 my-5 md:my-10 text-gray-300">
 			<div className="text-2xl md:text-4xl font-black">
-				I'm Kelechukwu Ikechukwu
+				<SpringyText
+					className="justify-center"
+					text="I'm Kelechukwu Ikechukwu"
+				/>
 			</div>
-			<div className="text-xl">A Frontend Engineer</div>
+			<div className="text-xl flex justify-center md:justify-start">
+				A Frontend Engineer
+			</div>
 		</div>
 	);
 };
