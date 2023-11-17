493d3a0800feab0952b43c32249c997b987c2709
Kelechukwu <paparazzip8@gmail.com>
2023-11-17 22:34:36 +0100

    updated project archive images and links

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -10,51 +10,44 @@ const Projects = () => {
 	const projects = [
 		{
 			id: 1,
-			name: "projectOne",
+			name: "Fly Asap",
 			tools: "NextJs, TailwindCss",
-			image: "/setup.jpg",
-			github_link: "https://www.github.com/kelechukwu1",
+			image: "/getlinked.png",
+			github_link: "https://github.com/kelechukwu1/flight-reservation-website",
 			live_link: "https://www.flight-reservation-project.vercel.app",
-			role: "role",
 			description:
 				"This is a web application which allows users to book and pay for thier flights seemlessly.",
-			deployed_link: "deployed_link",
 		},
 		{
 			id: 2,
-			name: "projectTWo",
-			tools: "NextJs, TailwindCss",
-			image: "/setup.jpg",
-			github_link: "https://www.github.com/kelechukwu1",
-			live_link: "https://www.flight-reservation-project.vercel.app",
-			role: "role",
+			name: "GetLinkedAI",
+			tools: "NextJs, TailwindCss, GSAP, AOS.js",
+			image: "/getlinked.png",
+			github_link: "https://github.com/kelechukwu1/getlinkedAI",
+			live_link: "https://www.getlinked-ai-psi.vercel.app",
 			description:
-				"This is a web application which allows users to book and pay for thier flights seemlessly.",
-			deployed_link: "deployed_link",
+				"A 5-days hackathon project I participated, built and completed with Ayoola my good friend, we ensured maximum responsiveness and built beautiful components and pages, also played around with getLinkedAI API",
 		},
 		{
 			id: 3,
-			name: "projectThree",
+			name: "Remote Mobile Fix",
 			tools: "NextJs, TailwindCss",
-			image: "/KC.jpeg",
-			github_link: "https://www.github.com/kelechukwu1",
-			role: "null",
+			image: "/remotefix.png",
+			github_link: "https://github.com/kelechukwu1/remote-mobile-fix",
 			description:
 				"This web app enables users in Owerri (capital of Imo State, Nigeria) to locate phone repairers close to thier location.",
-			deployed_link: "https://www.remote-mobile-fix.vercel.app",
+			live_link: "https://www.remote-mobile-fix.vercel.app",
 		},
 
 		{
 			id: 4,
-			name: "projectFour",
+			name: "Abeg Help",
 			tools: "NextJs, TailwindCss",
-			image: "/setup.jpg",
-			github_link: "https://www.github.com/kelechukwu1",
-			live_link: "https://www.flight-reservation-project.vercel.app",
-			role: "role",
+			image: "/getlinked.png",
+			github_link: "https://www.github.com/kelechukwu1/abeg-help",
+			live_link: "https://www.abeg-help.vercel.app",
 			description:
-				"This is a web application which allows users to book and pay for thier flights seemlessly.",
-			deployed_link: "deployed_link",
+				"This is a community project I'm participating actively on currently to solve issues with fund raising in Nigeria",
 		},
 	];
 
@@ -72,70 +65,128 @@ const Projects = () => {
 					Each one is unique. Take a look.
 				</div>
 			</div>
+
+			{/* large screen design */}
+			<div className="hidden lg:block">
+				<div className="grid lg:grid-cols-3 items-center justify-center gap-16 mb-20">
+					<div className="justify-end col-span-2">
+						<Image
+							src={"/setup.jpg"}
+							alt={"setup screenshot"}
+							className="
+                pointer-events-none
+                rounded-md
+                duration-400
+                ease-in-out
+                group-hover:scale-105
+                w-full h-[20rem]"
+							width={900}
+							height={10}
+						/>
+					</div>
+					<div className="space-y-5 col-span-1">
+						<div className="flex lg:gap-3 items-center">
+							<div className="text-xl text-gray-300">01</div>
+							<div className="text-gray-100 font-bold text-3xl">Fly ASAP</div>
+						</div>
+
+						<div className="text-gray-300 text-muted-main fluid-lg">
+							<div>
+								This is a web application which allows users to book and pay for
+								thier flights seemlessly.
+							</div>
+							<div>Stack/Tools : NextJs, TailwindCss</div>
+						</div>
+						<div className="flex gap-5 items-center">
+							<div>
+								<Link
+									// onMouseEnter={() => githubProjectCursor()}
+									// onMouseLeave={() => projectCursor()}
+									rel="noopener noreferrer"
+									target="_blank"
+									href={"https://www.github.com/kelechukwu1"}
+								>
+									<AiOutlineGithub className="text-3xl text-gray-200" />
+								</Link>
+							</div>
+							<div>
+								<Link
+									// onMouseEnter={() => githubProjectCursor()}
+									// onMouseLeave={() => projectCursor()}
+									rel="noopener noreferrer"
+									target="_blank"
+									href={"https://www.flight-reservation-system.vercel.app"}
+								>
+									<PiArrowSquareOutBold className="text-3xl text-gray-200" />
+								</Link>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div className="grid lg:grid-cols-3 items-center justify-center gap-16 mb-20">
+					<div className="space-y-5 col-span-1">
+						<div className="flex lg:gap-3 items-center">
+							<div className="text-xl text-gray-300">02</div>
+							<div className="text-gray-100 font-bold text-3xl">
+								RemoteMobile Fix
+							</div>
+						</div>
+
+						<div className="text-gray-300 text-muted-main fluid-lg">
+							<div>
+								This is a web application which allows users to book and pay for
+								thier flights seemlessly.
+							</div>
+							<div>Stack/Tools : NextJs, TailwindCss</div>
+						</div>
+						<div className="flex gap-5 items-center">
+							<div>
+								<Link
+									// onMouseEnter={() => githubProjectCursor()}
+									// onMouseLeave={() => projectCursor()}
+									rel="noopener noreferrer"
+									target="_blank"
+									href={"https://www.github.com/kelechukwu1"}
+								>
+									<AiOutlineGithub className="text-3xl text-gray-200" />
+								</Link>
+							</div>
+							<div>
+								<Link
+									// onMouseEnter={() => githubProjectCursor()}
+									// onMouseLeave={() => projectCursor()}
+									rel="noopener noreferrer"
+									target="_blank"
+									href={"https://www.flight-reservation-system.vercel.app"}
+								>
+									<PiArrowSquareOutBold className="text-3xl text-gray-200" />
+								</Link>
+							</div>
+						</div>
+					</div>
+					<div className="justify-end col-span-2">
+						<Image
+							src={"/setup.jpg"}
+							alt={"setup screenshot"}
+							className="
+                pointer-events-none
+                rounded-md
+                duration-400
+                ease-in-out
+                group-hover:scale-105
+                w-full h-[20rem]"
+							width={900}
+							height={10}
+						/>
+					</div>
+				</div>
+			</div>
+
 			<ul>
 				<li>
 					{projects.map((project) => (
-						/* large screen design */
 						<div>
-							<div className="hidden lg:block">
-								<div className="grid lg:grid-cols-3 items-center justify-center gap-16">
-									<div className="justify-end col-span-2 mb-20">
-										<Image
-											src={project.image}
-											alt={`${project.image} screenshot`}
-											className="
-                    pointer-events-none
-                    rounded-md
-                    duration-400
-                    ease-in-out
-                    group-hover:scale-105
-                    w-full h-[20rem]"
-											width={900}
-											height={10}
-										/>
-									</div>
-									<div className="space-y-5 col-span-1">
-										<div className="text-xl text-gray-300">01</div>
-										<div className="text-gray-100 text-3xl noto">
-											{project.name}
-										</div>
-										<div className="text-gray-300 text-muted-main fluid-lg">
-											<div>
-												This is a web application which allows users to book and
-												pay for thier flights seemlessly.
-											</div>
-											<div>Stack/Tools : {project.tools}</div>
-										</div>
-										<div className="flex gap-5 items-center">
-											<div>
-												<Link
-													// onMouseEnter={() => githubProjectCursor()}
-													// onMouseLeave={() => projectCursor()}
-													rel="noopener noreferrer"
-													target="_blank"
-													href={"https://www.github.com/kelechukwu1"}
-												>
-													<AiOutlineGithub className="text-3xl text-gray-200" />
-												</Link>
-											</div>
-											<div>
-												<Link
-													// onMouseEnter={() => githubProjectCursor()}
-													// onMouseLeave={() => projectCursor()}
-													rel="noopener noreferrer"
-													target="_blank"
-													href={
-														"https://www.flight-reservation-system.vercel.app"
-													}
-												>
-													<PiArrowSquareOutBold className="text-3xl text-gray-200" />
-												</Link>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-
 							{/* mobile design */}
 							<div className="lg:hidden mb-20">
 								<div className="items-center justify-center">
@@ -149,19 +200,19 @@ const Projects = () => {
                         duration-400
                         ease-in-out
                         group-hover:scale-105
-                        w-full h-[15rem]"
+                        w-full h-[100%]"
 											width={900}
 											height={10}
 										/>
 									</div>
-									<div className="space-y-5 col-span-1 mt-10">
-										<div className="flex text-xl text-gray-300 items-center gap-2 noto">
+									<div className="space-y-3 col-span-1 mt-5">
+										<div className="flex text-md text-gray-300 items-center gap-1">
 											{`0${project.id}`}
-											<span className="text-gray-100 text-muted-main fluid-lg noto">
+											<span className="text-gray-100 text-xl font-semibold">
 												{project.name}
 											</span>
 										</div>
-										<div className="text-gray-300 text-xl">
+										<div className="text-gray-300 text-muted-main fluid-lg">
 											<div className=" ">{project.description}</div>
 											<div className=" ">Stack/Tools : {project.tools}</div>
 										</div>
@@ -203,3 +254,66 @@ const Projects = () => {
 };
 
 export default Projects;
+
+{
+	/* {projects.map((project) => (
+				<ul className="hidden lg:block">
+					<li id={project.id}>
+						<div className="grid lg:grid-cols-3 items-center justify-center gap-16 mb-20">
+							<div className="justify-end col-span-2">
+								<Image
+									src={project.image}
+									alt={"setup screenshot"}
+									className="
+                pointer-events-none
+                rounded-md
+                duration-400
+                ease-in-out
+                group-hover:scale-105
+                w-full h-[20rem]"
+									width={900}
+									height={10}
+								/>
+							</div>
+							<div className="space-y-5 col-span-1">
+								<div className="flex lg:gap-3 items-center">
+									<div className="text-xl text-gray-300">01</div>
+									<div className="text-gray-100 font-bold text-3xl">
+										{project.name}
+									</div>
+								</div>
+
+								<div className="text-gray-300 text-muted-main fluid-lg">
+									<div>{project.description}</div>
+									<div>Stack/Tools : {project.tools}</div>
+								</div>
+								<div className="flex gap-5 items-center">
+									<div>
+										<Link
+											// onMouseEnter={() => githubProjectCursor()}
+											// onMouseLeave={() => projectCursor()}
+											rel="noopener noreferrer"
+											target="_blank"
+											href={project.github_link}
+										>
+											<AiOutlineGithub className="text-3xl text-gray-200" />
+										</Link>
+									</div>
+									<div>
+										<Link
+											// onMouseEnter={() => githubProjectCursor()}
+											// onMouseLeave={() => projectCursor()}
+											rel="noopener noreferrer"
+											target="_blank"
+											href={project.live_link}
+										>
+											<PiArrowSquareOutBold className="text-3xl text-gray-200" />
+										</Link>
+									</div>
+								</div>
+							</div>
+						</div>
+					</li>
+				</ul>
+			))} */
+}
